fb294e89811f61ac5be699a49dbc77e89880f049
Jingqi Zhou <theJingqiZhou@gmail.com>
2026-02-14 15:53:39 +0800

    refactor(api): Expose recent impls at models level

--- tf2_eegmodels/models/__init__.py
+++ tf2_eegmodels/models/__init__.py
@@ -1,4 +1,7 @@
+from .brainnet import BrainNet
 from .deepconvnet import DeepConvNet
 from .eegnet import EEGNet
 from .eegnet_v4 import EEGNetV4
 from .shallowconvnet import ShallowConvNet
+from .ssvepformer import SSVEPFormer
+from .taskorinet import TaskOriNet
